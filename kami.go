9aeb10a512de1a2523ce7e74442e2a6ecf8b977b
erukiti <erukiti@gmail.com>
2015-12-28 03:12:40 +0900

    create Monitor class

--- kami.go
+++ kami.go
@@ -10,20 +10,35 @@ import (
 	"fmt"
 )
 
-
-
 type Rule struct {
-	Args        []string
-	WorkingDir  string
-	LogStdout   string
-	LogStderr   string
-	Env         []string
+	Name         string
+	Args         []string
+	WorkingDir   string
+	LogDirStdout string
+	LogDirStderr string
+	Env          []string
 }
 
-func redirect(dst string, src io.Reader) (err error) {
+type Opt struct {
+	// StdoutTo    io.Writer
+	// StderrTo    io.Writer
+	// StdinFrom   io.Reader
+}
+
+type Monitor struct {
+	name   string
+	env    []string
+	Pid    int
+	stdout io.Reader
+	stderr io.Reader
+}
+
+func (m *Monitor) redirect(dstDir string, src io.Reader) {
 	go func() {
+		dst := fmt.Sprintf("%s/%s-%d.log", dstDir, m.name, m.Pid)
 		outfile, err := os.Create(dst)
 		if err != nil {
+			fmt.Println(err)
 			return
 		}
 		defer outfile.Close()
@@ -32,62 +47,64 @@ func redirect(dst string, src io.Reader) (err error) {
 		defer writer.Flush()
 
 		io.Copy(writer, src)
+		return
 	}()
-	return
 }
 
-func Monitor(rule Rule) {
-	c := exec.Command(rule.Args[0], rule.Args[1:]...)
-	if rule.WorkingDir != "" {
-		c.Dir = rule.WorkingDir
-	}
-
-	c.Env = rule.Env
-	f, e, err := pty.Start2(c)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-	}
-
-	if (rule.LogStdout != "") {
-		redirect(rule.LogStdout, f)
-	}
-
-	if (rule.LogStderr != "") {
-		redirect(rule.LogStderr, e)
-	}
-
-	util.Dump(c.Process.Pid)
-	// go func() {
+func (m *Monitor) run(rule Rule) {
+	var err error
+	go func (){
 		for {
+			c := exec.Command(rule.Args[0], rule.Args[1:]...)
+			if rule.WorkingDir != "" {
+				c.Dir = rule.WorkingDir
+			}
+
+			c.Env = m.env
+			m.stdout, m.stderr, err = pty.Start2(c)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			if (rule.LogDirStdout != "") {
+				m.redirect(rule.LogDirStdout, m.stdout)
+			}
+
+			if (rule.LogDirStderr != "") {
+				m.redirect(rule.LogDirStderr, m.stderr)
+			}
+
+			m.Pid = c.Process.Pid
+
 			state, err := c.Process.Wait()
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "? process wait error: %s\n", err)
+				fmt.Println(err)
 				return
 			} else if state.Exited() {
-				util.Dump(state.Exited())
-				util.Dump(state.Pid())
-				util.Dump(state.String())
-				util.Dump(state.Success())
-				util.Dump(state.Sys())
-				util.Dump(state.SysUsage())
-				util.Dump(state.SystemTime())
-				util.Dump(state.UserTime())
-				fmt.Println("exit")
-				return
+				continue
 			}
 			util.Dump(state)
 		}
-	}
+	}()
+}
 
+func Create(rule Rule) (m *Monitor, err error) {
+	m = &Monitor{}
+	m.name = rule.Name
+	m.env = append(os.Environ(), rule.Env...)
 
+	m.run(rule)
+	for {}
 
+	return m, nil
+}
 
-	func main() {
-		rule := Rule{}
-		rule.Args = []string{"env"}
-		rule.WorkingDir = ".."
-		rule.LogStdout = "hoge.txt"
-		rule.Env = append(os.Environ(), "HOGE=FUGA")
+func main() {
+	rule := Rule{}
+	rule.Args = []string{"env"}
+	rule.WorkingDir = ""
+	rule.Env = []string{"HOGE=FUGA"}
 
-		Monitor(rule)
-	}
+	Create(rule)
+}
